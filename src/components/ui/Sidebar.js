d2700aa796e67c9da8b7fdab7943d2016efa0452
Miguel5858 <jmti4884@gmail.com>
2022-02-05 21:46:17 -0500

    first commit

---
+++ src/components/ui/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Sidebar = () => {
+    return ( 
+        <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
+            <div className="p-6">
+                <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">Administrador</p>
+
+                <p className="mt-3 text-gray-600 ">Administra tus productos en las siguientes opciones:</p>
+
+                <nav className="mt-10">
+                    <NavLink className="p-1 text-gray-400 block hover:bg-green-500 hover:text-gray-900 rounded" activeClassName="text-yellow-500" exact="true" to="/">Ordenes</NavLink>
+                    <NavLink className="p-1 text-gray-400 block hover:bg-green-500 hover:text-gray-900 rounded" activeClassName="text-yellow-500" exact="true" to="/menu">Menú</NavLink>
+                </nav>
+            </div>
+        </div>
+     );
+}
+ 
+export default Sidebar;
